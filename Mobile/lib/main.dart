d77bdddd56e04e3838144daa50de561c1917e7d0
Murat Alperen Ulutaş <153949398+muratalperenulutas@users.noreply.github.com>
2024-10-15 18:44:31 +0300

    Horizontal mode has been disabled

--- Mobile/lib/main.dart
+++ Mobile/lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:word_app/app.dart';
 import 'package:word_app/services/randomWordService/random_word.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   RandomWordService().initialize;
   //RandomWordService().getRandomWords(3,"oxfordA1");
-
-  runApp(MyApp());
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((_) {
+    runApp(MyApp());
+  });
 }
